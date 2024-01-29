04f4f07ff4525d1062a8c664e676fd5456ff91f2
maleekanarjisrizvi <maleekanarjjis1515@gmail.com>
2024-01-29 12:19:56 +0500

    Main class changes comitted

---
+++ HotelRoom.cpp
@@ -0,0 +1,21 @@
+class Customers{
+
+public void searchmodule();
+public void Hoteldetailsmodule(); 
+public void checkavailability();
+public void payment();
+
+
+
+
+}
+
+
+
+class Hotel
+{
+public void bookrooms(); 
+public void hoteldata();
+public void Bookingdata();
+
+}
